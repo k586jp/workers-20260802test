3d57ac2cca6675b9764b8acf56144179351b1749
k586 <mail@k586.jp>
2026-08-10 01:42:04 +0900

    test

--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,7 @@ async function articleListHtml(context: c) {
 async function articleHtml(context: c) {;
     const id = context.req.param('id');
     const json: Article[] = await context.env.K586_ARTICLE_ID.getArticle(id);
-    return context.html(PageLayout(json[0]));
+    return context.html(PageLayout(json));
 }
 
 // ================================================================
